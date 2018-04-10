e5fdf8a049ec56ccdd2971f4d2c3dca0e86b356b
Dustin Myers <dustint314@gmail.com>
2018-04-09 20:47:33 -0600

    WIP working on emails

--- app/routes/middleware.js
+++ app/routes/middleware.js
@@ -11,9 +11,6 @@
 'use strict';
 
 const _ = require('lodash');
-const passport   = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
 
 // /**
 // 	Initialises the standard view locals
