e756b99f2273786f4e18b8aaded6d6d47c1f5fff
Dustin Myers <dustint314@gmail.com>
2018-05-10 09:55:58 -0600

    Add icon types to pressItems

--- app/routes/press.js
+++ app/routes/press.js
@@ -4,31 +4,35 @@
 const each = require('lodash').each;
 
 module.exports = (_app) => {
+  function getPressItemIcons(items) {
+    each(items, pressItem => {
+      switch(pressItem.fields.type) {
+        case 'appearance':
+          pressItem.typeIcon = 'icon-camera';
+          break;
+        case 'interview':
+          pressItem.typeIcon = 'icon-mic';
+          break;
+        case 'review':
+          pressItem.typeIcon = 'icon-newspaper';
+          break;
+        default:
+          pressItem.typeIcon = 'icon-document';
+          break;
+      }
+    });
+
+    return items;
+  }
+
 	_app.app.get('/press', (req, res) => {
 		let content = _app.content;
-		let pressItems  = content.entries.pressItem;
+		let pressItems  = getPressItemIcons(content.entries.pressItem);
 		let galleries   = content.entries.gallery;
 		let pressPage = content.pages.pressMediaPage;
 		let locals      = res.locals;
 		locals.section  = 'publications';
 		locals.siteName = 'David Tomas Martinez';
-
-		each(pressItems, pressItem => {
-			switch(this.type) {
-				case 'appearance':
-					pressItem.typeIcon = 'icon-camera';
-					break;
-				case 'interview':
-					pressItem.typeIcon = 'icon-mic';
-					break;
-				case 'review':
-					pressItem.typeIcon = 'icon-newspaper';
-					break;
-				default:
-					pressItem.typeIcon = 'icon-document';
-					break;
-			}
-		});
 
 		let renderArgs = {
 			layout: 'page',
